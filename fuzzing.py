f7bf9fef912f342031e9c0345490ee45a61f3121
Yunus <yunustemre7@gmail.com>
2020-08-01 09:04:06 -0400

    Add files via upload

---
+++ fuzzing.py
@@ -0,0 +1,17 @@
+import requests
+
+file = open("fuzzing.txt", "r")
+content = file.read()
+file.close()
+
+header = {"Cookie": "security=low; PHPSESSID=..................."}
+
+for i in content.splitlines():
+    print(i)
+    url = "http://www.google.com" + str(i)
+    result = requests.get(url= url, headers= header)
+
+    if "280" in str(result.status_code):
+        print(url + " exists!")
+    else:
+        print("No file or directory.")
